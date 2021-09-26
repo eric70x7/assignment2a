6efd931ecb819b5f5ed5383ff2935b5fababa192
Eric Jones <eric70x7@gmail.com>
2021-09-26 10:29:30 -0700

    Starting

---
+++ content/TestForm.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>TestForm</title>
+</head>
+<body>
+<form action="TestServlet" method="post">
+First: <input type="text" name="firstName" placeholder="First Name" />
+Last: <input type="text" name="lastName" placeholder="Last Name" />
+<input type = "submit" value = "Submit" />
+</body>
+</html>
---
+++ source/testServlet/TestServlet.java
@@ -0,0 +1,70 @@
+package testServlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet");
+		// TODO Auto-generated method stub
+
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		
+		System.out.println(firstName);
+		System.out.println(lastName);
+		
+		request.setAttribute("firstName", firstName);
+		request.setAttribute("lastName", lastName);
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("TestResponse.jsp");
+		requestDispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost");
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
